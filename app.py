3de640bd1a361ebe1f68925aeaf75a54b3d33382
marwa-werghemi <marwaouerghemmi60@gmail.com>
2025-06-01 12:27:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1344,7 +1344,7 @@ else:
     st.info("Aucune donnée disponible à afficher.")
 
   # ℹ️ Aide et légende
-  if st.session_state.role in ["admin", "manager"]:
+if st.session_state.role in ["admin", "manager"]:
     with st.expander("ℹ️ Aide et légende"):
         st.markdown("""
         **Légende des couleurs :**
